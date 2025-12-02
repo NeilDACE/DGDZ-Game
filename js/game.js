eb7a666e2a57320096880971fa337cc3a6735010
Mr.Orange88 <m.orange@gmx.de>
2025-12-02 17:35:02 +0100

    feat: remove placeholder text from index.html and update game scenes configuration

--- js/game.js
+++ js/game.js
@@ -3,7 +3,7 @@ const config = {
   width: 800,
   height: 600,
   backgroundColor: "#000000",
-  scene: [StartScene, LevelScene],
+  scene: [StartScene, StoryScene],
 };
 
 const game = new Phaser.Game(config);
